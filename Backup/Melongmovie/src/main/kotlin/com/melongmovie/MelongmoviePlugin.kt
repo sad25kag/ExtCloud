853ea115ad46c2a2ca754d066cfa7cc3a9a48a64
sad25kag <sad25kag@gmail.com>
2026-05-11 09:47:43 +0700

    backup

---
+++ Backup/Melongmovie/src/main/kotlin/com/melongmovie/MelongmoviePlugin.kt
@@ -0,0 +1,16 @@
+package com.melongmovie
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class MelongmoviePlugin : Plugin() {
+    override fun load(context: Context) {
+        Melongmovie.context = context
+        registerMainAPI(Melongmovie())
+        registerExtractorAPI(Dingtezuni())
+        registerExtractorAPI(Melongfilmstrp2p())
+        registerExtractorAPI(Dintezuvio())
+    }
+}
